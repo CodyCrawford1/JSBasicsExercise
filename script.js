1d33d995e4ed0a2b0bd26c3cd45b5166f94b5e40
Cody Crawford <Cody.Crawford1010@gmail.com>
2022-02-11 14:32:51 -0600

    Completed Exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(1);
+// The five main data types are Numbers, Strings, Booleans, Null, and Undefined
+// The six operator symbols are +, -, *, /, **, %
+console.log(5 % 4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "eye");
+console.log("Cody Crawford".length);
+console.log("Avengers: Endgame"[17]);
